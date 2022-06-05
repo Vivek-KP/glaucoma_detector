417481ffb12ee31a0803e18ced3d36b027266155
Vivek <44610017+Vivek-KP@users.noreply.github.com>
2022-06-05 23:06:38 +0530

    Add files via upload

---
+++ glucoma/main.py
@@ -0,0 +1,74 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
+import requests
+import os
+from keras.models import load_model
+import numpy as np
+from PIL import Image, ImageOps
+from werkzeug.utils import secure_filename
+ 
+app = Flask(__name__)
+ 
+UPLOAD_FOLDER = 'static/uploads/'
+ 
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+     
+ 
+@app.route('/')
+def home():
+    return render_template('base.html')
+ 
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+    
+        model = load_model('static/model/keras_model.h5')
+        data = np.ndarray(shape=(1, 224, 224,3), dtype=np.float32)
+        # Replace this with the path to your image
+        image = Image.open('static/uploads/'+file.filename)
+        #resize the image to a 224x224 with the same strategy as in TM2:
+        #resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+        #turn the image into a numpy array
+        image_array = np.asarray(image)
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+        # Load the image into the array
+        data[0] = normalized_image_array
+        
+        # run the inference
+        prediction = model.predict(data)
+        v=prediction.argmax(axis=1)[0]
+        cla = ["0.2-0.4 and glaucoma negative","0.41-0.6 and glaucoma negative and may become positive in future","0.61-0.75 and glaucoma positive","0.75-above and having severe glaucoma"]
+        r=cla[v]
+        return render_template('index.html', r=r)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+ 
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+ 
+if __name__ == "__main__":
+    app.run()
